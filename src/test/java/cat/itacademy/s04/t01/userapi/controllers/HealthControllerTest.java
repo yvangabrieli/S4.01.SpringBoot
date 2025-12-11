5f80eb2f3f6fd56fc34a5315234211f4a9ec9cc0
Yvan Gabrieli <yvan.gabrielik@gmail.com>
2025-12-11 10:26:55 +0100

    test : verify /heath returns JSON status OK

---
+++ src/test/java/cat/itacademy/s04/t01/userapi/controllers/HealthControllerTest.java
@@ -0,0 +1,25 @@
+package cat.itacademy.s04.t01.userapi.controllers;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HealthController.class)
+public class HealthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldReturnOkStatus() throws Exception {
+        mockMvc.perform(get("/health"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("OK"));
+    }
+
+}
