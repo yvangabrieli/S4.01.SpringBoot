4d048843de7bd17bc35bb9c07a7862ffb4d54434
Yvan Gabrieli <yvan.gabrielik@gmail.com>
2025-12-17 12:44:52 +0100

    fix(user-controller): integrate UserService and remove direct repository access

--- src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
@@ -2,43 +2,46 @@ package cat.itacademy.s04.t01.userapi.controllers;
 
 
 import cat.itacademy.s04.t01.userapi.models.User;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
+import cat.itacademy.s04.t01.userapi.service.UserService;
 
-import java.util.ArrayList;
+import org.springframework.web.bind.annotation.*;
+
+
+
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
-import java.util.stream.Collectors;
+
 
 @RestController
 @RequestMapping ("/users")
 public class UserController {
-    private static List<User> users = new ArrayList<>();
+    private final UserService userService;
 
-    @GetMapping
-    public List<User> getUsers(){
-        return users;
+    public UserController (UserService userService){
+        this.userService = userService;
     }
+
+
 
     @PostMapping
-    public User createUser (@RequestBody User user){
-        user.setId(UUID.randomUUID());
-        users.add(user);
-        return user;
+    public User create (@RequestBody User user){
+        return userService.create(user);
     }
+
+    @GetMapping
+    public List<User> getAll(@RequestParam(required = false) String name){
+
+        return userService.findAll();
+    }
+
     @GetMapping("/{id}")
-    public User getUserById(@PathVariable UUID id) {
-        return users.stream()
-                .filter(u -> u.getId().equals(id))
-                .findFirst()
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    public Optional<User> getById(@PathVariable UUID id) {
+        return userService.findById(id);
     }
 
     @GetMapping(params = "name")
-    public List<User> getUsersByName(@RequestParam String name) {
-        return users.stream()
-                .filter(u -> u.getName().toLowerCase().contains(name.toLowerCase()))
-                .collect(Collectors.toList());
+    public Optional<User> getUsersByName(@RequestParam String name) {
+        return userService.findByName(name);
     }
 }
