149233443767661f0fdc305807f487f76b4a8562
Yvan Gabrieli <yvan.gabrielik@gmail.com>
2025-12-17 12:42:32 +0100

    feat(user-service): add UserNotFoundException for missing user handling

---
+++ src/main/java/cat/itacademy/s04/t01/userapi/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package cat.itacademy.s04.t01.userapi.exception;
+
+import java.util.UUID;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(UUID id){
+        super("User with id: " + id + " , is not found" );
+    }
+}
