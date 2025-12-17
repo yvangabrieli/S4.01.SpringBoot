6e59f603dda1879032561aa5b808d362b27ab71a
Yvan Gabrieli <yvan.gabrielik@gmail.com>
2025-12-17 12:41:09 +0100

    feat(user-repository): implement InMemoryUserRepository for in-memory data storage

---
+++ src/main/java/cat/itacademy/s04/t01/userapi/repository/InMemoryUserRepository.java
@@ -0,0 +1,40 @@
+package cat.itacademy.s04.t01.userapi.repository;
+
+import cat.itacademy.s04.t01.userapi.models.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+@Repository
+public class InMemoryUserRepository implements UserRepository {
+    private final List<User> users = new ArrayList<>();
+
+@Override
+    public User save (User user){
+    users.add(user);
+    return user;
+}
+@Override
+    public List<User> findAll(){
+    return users;
+}
+    @Override
+    public Optional<User> findById(UUID id){
+        return users.stream()
+                .filter(u->u.getId().equals(id))
+                .findFirst();
+    }
+    @Override
+    public Optional <User> searchByName(String name){
+        return users.stream()
+                .filter(u->u.getName().toLowerCase().contains(name.toLowerCase()))
+                .findFirst();
+    }
+    @Override
+    public boolean existsByEmail(String email){
+        return users.stream()
+                .anyMatch(u->u.getEmail().equalsIgnoreCase(email));
+    }
+}
