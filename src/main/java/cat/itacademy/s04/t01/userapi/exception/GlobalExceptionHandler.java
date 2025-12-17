7830da6f94c85b9d4795ba8d27ba909584b482c0
Yvan Gabrieli <yvan.gabrielik@gmail.com>
2025-12-17 12:42:51 +0100

    feat(user-service): add GlobalExceptionHandler to handle custom exceptions globally

---
+++ src/main/java/cat/itacademy/s04/t01/userapi/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package cat.itacademy.s04.t01.userapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.Map;
+
+public class GlobalExceptionHandler {
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public Map<String, String> handleEmailExists(EmailAlreadyExistsException ex) {
+        return Map.of("error", ex.getMessage());
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handleUserNotFound(UserNotFoundException ex) {
+        return Map.of("error", ex.getMessage());
+    }
+}
+
